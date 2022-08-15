a6ac8959212703ac1cf76a453b2e3d7c4db346d7
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-08-15 10:18:48 -0500

    :recycle: Update

--- errors.zig
+++ errors.zig
@@ -68,3 +68,21 @@ test "errdefer" {
         return;
     };
 }
+
+fn createFile() !void {
+    return error.AccessDenied;
+}
+
+test "inferred error set" {
+    //type coercion successfully takes place
+    const x: error{AccessDenied}!void = createFile();
+
+    //Zig does not let us ignore error unions via _ = x;
+    //we must unwrap it with "try", "catch", or "if" by any means
+    _ = x catch {};
+}
+
+const A = error{ NotDir, PathNotFound };
+const B = error{ OutOfMemory, PathNotFound };
+
+const C = A || B;
